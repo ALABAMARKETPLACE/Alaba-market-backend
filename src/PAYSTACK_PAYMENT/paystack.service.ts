0a954e97575ec4409c797afb964def7534fe9d32
Tollyboy2 <supportng1@taxgoglobal.com>
2025-12-21 20:38:37 -0800

    Fix paystack syntax error

--- src/PAYSTACK_PAYMENT/paystack.service.ts
+++ src/PAYSTACK_PAYMENT/paystack.service.ts
@@ -26,7 +26,7 @@ import { PaystackWebhookDto } from "./dto/paystack-webhook.dto";
 
 import { Store } from "../STORE/store.entity";
 import { PaymentSplitService } from "../PAYMENT_SPLITS/payment-split.service";
-import { OrderPayments } from "src/ORDER_PAYMENTS/order_payments.entity";
+import { OrderPayments } from "../ORDER_PAYMENTS/order_payments.entity";
 import { OrderStatus } from "src/ORDER_STATUS/order_status.entity";
 import { Order } from "src/ORDER/order.entity";
 
