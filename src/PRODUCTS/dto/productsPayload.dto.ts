0fe97f16ff15d8812966bbb7949a643eac2298a9
Tollyboy2 <supportng1@taxgoglobal.com>
2026-01-08 02:40:25 -0800

    edit product service to add image: imageLocation to fix upload issue02

--- src/PRODUCTS/dto/productsPayload.dto.ts
+++ src/PRODUCTS/dto/productsPayload.dto.ts
@@ -2,20 +2,36 @@ import { ApiProperty } from "@nestjs/swagger";
 import { CreateProductsDto } from "./create.dto";
 import { CreateProductImageDto } from "../../PRODUCT_IMAGE/dto/createProductImage.dto";
 import { CreateProductVariantDto } from "../../PRODUCT_VARIANTS/dto/createProductVariant.dto";
+import { IsOptional, IsArray, ValidateNested } from "class-validator";
+import { Type } from "class-transformer";
 
 export class ProductsPayloadDto {
   @ApiProperty()
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => CreateProductsDto)
   readonly information: CreateProductsDto;
 
   @ApiProperty()
+  @IsOptional()
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => CreateProductImageDto)
   readonly images: CreateProductImageDto[];
 
   @ApiProperty()
+  @IsOptional()
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => CreateProductVariantDto)
   readonly variants: CreateProductVariantDto[];
 
   @ApiProperty()
-  readonly coverImage: CreateProductImageDto;
+  @IsOptional()
+  // Accept flexible shapes for coverImage (string url, object with url, or full CreateProductImageDto)
+  readonly coverImage: any;
 
   @ApiProperty()
+  @IsOptional()
   readonly product_video?: string;
 }
