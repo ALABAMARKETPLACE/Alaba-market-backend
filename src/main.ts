3e4134534eaeec24217e69466026599b4e1a326f
Tollyboy2 <supportng1@taxgoglobal.com>
2026-02-13 03:24:04 -0800

    pushed

--- src/main.ts
+++ src/main.ts
@@ -44,7 +44,7 @@ async function bootstrap() {
   );
   // ==================================================
 
-  // ================= BODY LIMITS ====================
+  // /================= BODY LIMITS ====================
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   // ==================================================
