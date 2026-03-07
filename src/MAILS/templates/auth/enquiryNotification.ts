b4d2c6beaeefa6f4f9da046ce85bef3fa45f3102
OLANIYI OLAMIDE <17261340+MrGiddz@users.noreply.github.com>
2026-03-07 19:17:43 +0100

    Correct mail template footer year

--- src/MAILS/templates/auth/enquiryNotification.ts
+++ src/MAILS/templates/auth/enquiryNotification.ts
@@ -83,7 +83,7 @@ const EnquiryNotification = (Details: any) => {
               </span>
             </h2>
             <p>
-               Founded in 2026 by the Alaba Amalgamated Traders Union in partnership with Taxgoglobal Corporation, 
+               Founded in 2025 by the Alaba Amalgamated Traders Union in partnership with Taxgoglobal Corporation, 
               ${process.env.NAME} leverages the power of marketing intelligence and e-commerce vision to deliver a wide range of products that make your lifestyle more attractive.
             </p>
           </div>
